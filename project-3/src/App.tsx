6d79b71097232f0c705d636b5d1783d8fa4061f0
Ingeborg Hirsti <ingeborghirsti@dhcp-10-22-14-158.wlan.ntnu.no>
2022-10-31 12:33:43 +0100

    add import

--- project-3/src/App.tsx
+++ project-3/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import PaginatedCountryList from './components/PaginatedCountryList'
 import Header from './components/Header'
 import MainPage from './components/MainPage'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 function App() {
 
