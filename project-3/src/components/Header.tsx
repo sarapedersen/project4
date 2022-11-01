8e0fc2575fd4e4d06f65797f6aec4d033f860916
Sara Holm Pedersen <saraped99@gmail.com>
2022-11-01 15:44:10 +0100

    add Header test

--- project-3/src/components/Header.tsx
+++ project-3/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense, useEffect, useState } from 'react'
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { searchCountryState, searchCountries, searchState, userState } from '../data/countryData';
+import { Link } from 'react-router-dom'
 
 
 function Header() {
@@ -23,9 +24,9 @@ function Header() {
                 </div>
                 <button onClick={() => setSearchCountries(query)} className="px-4 h-12 mt-20 md:mt-8 md:mb-6 md:px-8  rounded-lg bg-darkTeal text-white" type='button'>Search</button>
                 <div>
-                    <button type="button" className="inline-block absolute top-3 right-2 md:mt-6 md:mb-6 px-6 py-2.5 bg-transparent text-darkTeal md:text-white font-medium text-s leading-tight rounded 
+                    <Link to="/login"><button type="button" role="logout" className="inline-block absolute top-3 right-2 md:mt-6 md:mb-6 px-6 py-2.5 bg-transparent text-darkTeal md:text-white font-medium text-s leading-tight rounded 
                         hover:bg-gray-900 hover:bg-opacity-10 focus:outline-none focus:ring-0 active:bg-gray-900 active:bg-opacity-20 
-                        transition duration-150 ease-in-out">Log out</button>
+                        transition duration-150 ease-in-out">Log out</button></Link>
                 </div>
             </div>
         </div>
