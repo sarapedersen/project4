eb017f66787253bac00aa10c422827b5e2505e03
Mia Bjørndal <miabjo@stud.ntnu.no>
2022-11-17 12:55:38 +0100

    see darkmode and styling finished

--- frontend/src/pages/CountryPage.tsx
+++ frontend/src/pages/CountryPage.tsx
@@ -7,11 +7,11 @@ import lightModeImg from '../icons/lightmode.svg'
 
 
 function CountryPage() {
-    const bgStyle = 'min-h-screen bg-no-repeat bg-cover bg-center bg-fixed'
+    const bgStyle = ' min-h-screen bg-no-repeat bg-cover bg-center bg-fixed'
     const [darkmode, setDarkmode] = useRecoilState(darkMode)
-    
+
     return (
-        <div className={!darkmode ? ' md:bg-[url("./icons/background.svg")] bg-[url("./icons/background_mobile.svg")] ' + `${bgStyle}` : 'md:bg-[url("./icons/background_dark.svg")] bg-[url("./icons/background_dark_mobile.svg")]'+ `${bgStyle}`}>
+        <div className={!darkmode ? ' md:bg-[url("./icons/background.svg")] bg-bgBlue ' + `${bgStyle}` : 'md:bg-[url("./icons/background_dark.svg")] bg-bgBlack'+ `${bgStyle}`}>
             <Header/>
             <MainPage/>
         </div>
